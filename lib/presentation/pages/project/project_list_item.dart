db36b50d24a4a00c0611c73fbbb3aac571f68eae
Rifan <tubagusrifan@gmail.com>
2024-09-07 08:28:43 +0700

    perubahan tampilan card

--- lib/presentation/pages/project/project_list_item.dart
+++ lib/presentation/pages/project/project_list_item.dart
@@ -1,30 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../domain/entities/project_entity.dart';
-import '../../cubits/project/project_cubit.dart';
-import 'project_update.dart'; // Anda bisa membuat dialog update serupa dengan 'experience_update.dart'
 
 class ProjectListItem extends StatelessWidget {
   final ProjectEntity project;
 
-  const ProjectListItem({
-    required this.project,
-    super.key,
-  });
+  const ProjectListItem({super.key, required this.project});
 
   @override
   Widget build(BuildContext context) {
-    final auth = FirebaseAuth.instance;
-    final user = auth.currentUser;
-
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 10.0),
       child: ListTile(
-        onTap: () {
-          // Aksi yang dijalankan ketika item di-tap (contoh: detail project)
-          // Navigator.pushNamed(context, '/projectDetail', arguments: project);
-        },
         title: Text(
           project.title,
           style: const TextStyle(
@@ -34,38 +20,13 @@ class ProjectListItem extends StatelessWidget {
           ),
         ),
         subtitle: Text(
-          "Description: ${project.description.isNotEmpty ? project.description : 'No description available'}\n" // Tidak menggunakan ??
-          "Year: ${project.yearMade}\n"
-          "Built With: ${project.builtWith}\n"
-          "Made At: ${project.madeAt}\n"
-          "Link: ${project.link}",
+          project.description.isNotEmpty ? project.description : 'No description available',
+          style: const TextStyle(
+            fontSize: 14,
+            color: Colors.black,
+          ),
         ),
-        trailing: user != null
-            ? Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton(
-                    icon: const Icon(Icons.edit),
-                    onPressed: () {
-                      // Menampilkan dialog update project
-                      showDialog(
-                        context: context,
-                        builder: (context) => ProjectListUpdate(
-                          project: project,
-                        ),
-                      );
-                    },
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.delete),
-                    onPressed: () {
-                      // Menghapus project menggunakan ProjectCubit
-                      context.read<ProjectCubit>().removeProject(project.id);
-                    },
-                  ),
-                ],
-              )
-            : null,
+        // No trailing CRUD actions needed here, just basic information display
       ),
     );
   }
